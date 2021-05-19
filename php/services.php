64bd9a3a706cff7e0605214a4053e1756f88b2d5
therealmai <jomar35leano@gmail.com>
2021-05-19 12:05:44 +0800

    hard reset services

--- php/services.php
+++ php/services.php
@@ -26,80 +26,8 @@
     <br><br><br>
     </section>
 
-    <section style="padding-top: 50px; background-image: url(../img/pexels-pixabay-40784.jpg);background-repeat: no-repeat; background-size: 100%;">
-      <div class="page-wrap text-center">
-          <div style="padding-bottom: 5px;" >
-          </div>
-          <h1 style="font-size: 36px;color: #03045e;padding-bottom: 20px;"><strong> Here are the different services AquaPro provides</strong> </h1>
-      </div>
-      <div class="container">
-      <div class="card-deck">
-        <div class="card" style="text-align: center;">
-          <img src="../img/giphy.gif" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title"style="font-size: 28px;font-family: Roboto Condensed";><b>Water Quality Report</b></h5>
-            <p class="card-text"style="font-size: 20px;" >We <b>provide clean, safe and portable water</b> that is affordable for eveyone.</p>
-            <div style="margin-top: 47px;">
-              <a href="#report"> <button class="button2nd button2x">Learn More</button></a>
-        </div>
-          </div>
-      
-        </div>
-        <div class="card" style="text-align: center;">
-          <img src="../img/ezgif-7-94d28657b5e5.gif" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title" style="font-size: 28px;font-family: Roboto Condensed;"><b>Utilities</b></h5>
-            <p class="card-text" style="font-size: 20px;">Know our plans and what are the essential <b>services</b> that play a vital role in economic and social development.</p>
-            <div>
-              <a href="#utilities"><button class="button2nd button2x">Learn More</button></a> 
-        </div>
-          </div>
-        </div>
-        <div class="card" style="text-align: center;">
-          <img src="../img/new1.gif" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title"style="font-size: 28px;font-family: Roboto Condensed;"><b>Projects and Proposals</b></h5>
-            <p class="card-text" style="font-size: 20px;">We have <b>planned projects</b> to improve and exapnd our services for us to offer you only the best.</p>
-            <div style="margin-top: 40px;">
-              <a href="#projects"><button class="button2nd button2x">Learn More</button></a>
-        </div>
-          </div>
-        </div>
-      </div>
-    </div>
-      <br><br>
-  </section>
-  <section style="padding-top: 30px; background-color: #caf0f8;">
-      <div class="tab">
-      <button class="tablinks" onclick="openCity(event, 'London')">Quality Report</button>
-      <button class="tablinks" onclick="openCity(event, 'Paris')">Utilities</button>
-      <button class="tablinks" onclick="openCity(event, 'Tokyo')">Projects and Proposals</button>
-    </div>
-      
-          <div id="London" class="tabcontent">
-          <img src="../img/xwater-testing-905x509.jpg.pagespeed.ic.k9JZjQc_wk.jpg" align="left" width="550" style="padding-left:50px; margin: 0px 100px 0px 10px;"/>
-              <div style="text-align: justify; margin-right: 80px;">
-                  <h1 style="font-family: Roboto Condensed; font-size: 56px; font-weight: 500; color: #03045e;"><strong>Quality Report</strong> </h1>
-                  <p style="font-family: Roboto Condensed; font-size: 24px; padding-top: 20px;">Clean water demands sustainable industrialization, intact ecosystems and responsible consumption. Targeted, individual approaches are likely to have knock-on effects and unintended consequences, but an integrated approach will greatly enhance the chances of success of the goals.</p><br><br><br>
-              </div>
-          </div>
-
-          <div id="Paris" class="tabcontent">
-          <img src="../img/Water_Home.jpg" align="right" width="550" style="margin: 0px 50px 0px 50px;"/>
-               <div style="text-align: justify; margin-left: 65px;">
-                 <h1 style="font-family: Roboto Condensed; font-size: 56px; font-weight: 500; color: #03045e;"><strong>Utilities</strong> </h1>
-                <p style="font-family: Roboto Condensed; font-size: 24px; padding-top: 20px;">A well-run water utility is essential to people’s lives. Only the most efficient, financially viable utilities are able to respond to urban growth, connect the poor, and improve wastewater disposal practices.AquaPro utilities help to identify the capacity gaps in order to better serve customers and set themselves up for potential risks and/or shocks to the water supply and wastewater systems.</p><br>
-              </div>
-          </div>
-
-          <div id="Tokyo" class="tabcontent">
-           <img src="../img/project-work.jpg" align="left" width="550" style="padding-left:50px; margin: 0px 100px 0px 10px;"/>
-              <div style="text-align: justify; margin-right: 80px  ;">
-                <h1 style="font-family: Roboto Condensed; font-size: 56px; font-weight: 500; color: #03045e;"><strong>Projects and Proposals</strong> </h1>
-                <p style="font-family: Roboto Condensed; font-size: 24px; padding-top: 20px;">We have planned projects to improve and exapnd our services. Project Proposals contain key information about your project. They are essential for our benefactors since they’ll use them to evaluate our project and determine whether or not they’ll allocate funds for it.</p><br><br><br><br><br><br><br><br><br>
-              </div>
-          </div>
-      </section>
+   
+  
   <!-- <section style="padding-top: 60px; background-color: #caf0f8;">
     <div id="report">
     <img src="../img/xwater-testing-905x509.jpg.pagespeed.ic.k9JZjQc_wk.jpg" align="left" width="550" style="padding-left:50px; margin: 0px 100px 0px 10px;"/>
